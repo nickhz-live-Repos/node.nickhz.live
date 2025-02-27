c3f1d01a137d29a392ef7f857246f524bcd5ef48
hernandezn <hernandezn@live.com>
2025-02-27 12:25:35 -0800

    Removed Unused Static Site Modules

--- server-files/controller/origin.mjs
+++
@@ -1,6 +0,0 @@
-// serves requests for the static site in the _public
-import serve from '../util/serve-public-file.mjs';
-
-export default async (req, res) => {
-    await serve('/index.html', res);
-};
